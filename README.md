bd70c759349a348bd355a7c17527323daa2ee58c
Isaias <isaigrec@gmail.com>
2020-07-26 02:49:00 -0300

    Add message persistence and update README

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ API for a simple message chat via websocket.
 ## What is it?
 
 This Node application is a real time one to one chat via websockets that allow multiple channels conversation
-with another registered user. 
+with another registered user.
 
 ## How to run
 
@@ -24,13 +24,30 @@ or
 yarn dev
 ```
 
+You can also run the following commands to transpile the code to JavaScript version, that will be generated at the `/dist` folder in the projet root.
+
+```
+npm run build
+```
+or
+```
+yarn build
+```
+
 After that, go to the browser and type or click in the link above:
 
 http://localhost:3333/index.html
 
-To test just open two pages and register the users BEFORE send messages or the application will crash.
+To test just open two pages and register the users. Then, placing the user in the `To User` field the messages will arrive on the specified user chat.
 
+## Notes
+
+The messages persist until restarting the application. If you refresh the page and give the same user, the chat that already exists will appear after the registration.
+
+Chats are one to one, but not just two users. You can registry so many users (and open pages) you want. The messages will arive on the correct chat. An improvement possible is add group chats, what is easy to made just creating a custom room and joining the rescpetives scokets to them and use broadcast to a room. See the [Socket.Io documentation](https://socket.io/docs/rooms/)
+
+The messages are displayed in the same div, but this is just an example. If you want to use that on a best way you need to create chat tabs and handle the messages to show only the messages for that chat.
 
 ## Why?
 
-This is just a simple example for learning purpose. Feel free to use. 
+This is just a simple example for learning purpose. Feel free to use.
--- src/App.ts
+++ src/App.ts
@@ -13,7 +13,7 @@ class App {
     public server: Server
     private io: socketIo.Server
     private user_sockets: Array<{ user_id?: string, socket_id?: string }>
-    private messages: Array<{ user_id: string, messages: [{ from?: string, body?: string }]}>
+    private messages: Array<{ user_id: string, messages: [{ user_id?: string, msg?: string }]}>
 
     constructor() {
         this.express = express()
@@ -58,29 +58,37 @@ class App {
 
             socket.on('register', (msg: string) => {
                 const { user_id } = JSON.parse(msg)
-                console.log(user_id)
+
                 const registry = this.user_sockets.find(element => element.user_id === user_id)
+
                 if (registry) {
-                    console.log(this.messages)
                     registry.socket_id = socket.id
-                    socket.emit('previous', JSON.stringify(this.messages[user_id]))
+
+                    const user_history = this.messages.find(e => e.user_id === user_id)
+
+                    socket.emit('previous', JSON.stringify(user_history.messages))
                 } else {
                     this.user_sockets.push({ user_id: user_id, socket_id: socket.id })
-                    this.messages.push({ user_id: user_id, messages: [{ from: '', body: '' }] })
+                    this.messages.push({ user_id: user_id, messages: [{ user_id: '', msg: '' }] })
                 }
             })
 
-            socket.on('message', (msg: string) => {
-                const parsedMsg = JSON.parse(msg)
+            socket.on('message', (message: string) => {
+                const { user_id, to_user_id, msg } = JSON.parse(message)
 
-                const to_socket_id = this.user_sockets.find(element => element.user_id === parsedMsg.to_user_id)
+                const to_socket_id = this.user_sockets.find(element => element.user_id === to_user_id)
+
                 try {
-                    this.messages[parsedMsg.user_id].messages.push({ from: parsedMsg.user_id, body: parsedMsg.msg })
-                } catch (e) {
+                    const user_messages = this.messages.find(e => e.user_id === user_id)
+                    user_messages.messages.push({ user_id: user_id, msg: msg })
 
+                    const to_user_messages = this.messages.find(e => e.user_id === to_user_id)
+                    to_user_messages.messages.push({ user_id: user_id, msg: msg })
+                } catch (e) {
+                    console.log('Fail to add msg to history')
                 }
 
-                this.io.to(to_socket_id.socket_id).emit('received', { user_id: parsedMsg.user_id, msg: parsedMsg.msg })
+                this.io.to(to_socket_id.socket_id).emit('received', { user_id: user_id, msg: msg })
             })
 
             socket.on('disconnect', () => {
